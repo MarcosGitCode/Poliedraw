73b7452531f71cae1e0bf30450c9c796b6d48eda
DecoDamasio <demetriusbenatte@outlook.com>
2025-08-28 22:37:03 -0300

    update: Prompt e imagem redimensionada

--- JS/poliedraw.js
+++ JS/poliedraw.js
@@ -4,5 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     toggleBtn.addEventListener("click", () => {
         sidebar.classList.toggle("closed");
+        document.body.classList.toggle("sidebar-closed", sidebar.classList.contains("closed"));
+    });
+
+    document.querySelector('.estilo-btn').addEventListener('click', function() {
+        document.querySelector('.sidebar-top-buttons').style.display = 'none';
+    });
+    document.querySelector('.detalhes-btn').addEventListener('click', function() {
+        document.querySelector('.sidebar-top-buttons').style.display = 'none';
+    });
+
+    document.querySelector('.materias-btn').addEventListener('click', function() {
+        document.querySelector('.sidebar-top-buttons').style.display = 'flex';
     });
 });
