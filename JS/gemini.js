73188fd5dd0aba31be82e34248b09f089442406c
Guilherme Britto <guibrittotava@gmail.com>
2025-10-15 15:12:57 -0300

    fix: corrige erros pequenos

--- JS/gemini.js
+++ JS/gemini.js
@@ -3,12 +3,13 @@ import "dotenv/config";
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const ai = new GoogleGenAI({});
 
-export default async function Gemini(prompt) {
+export default async function Gemini() {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: prompt
+    contents: "Fala gemini, quanto é 43 + 43"
   });
+  console.log(response.text);
   return response.text;
 }
 
-main();
+Gemini();
