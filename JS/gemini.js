0cfdb0645db13bcc0e864d3d18a1b4891b2eabee
Guilherme Britto <guibrittotava@gmail.com>
2025-10-15 16:01:35 -0300

    feat: adiciona teste pratico do gemini (funcionou)

--- JS/gemini.js
+++ JS/gemini.js
@@ -1,15 +1,17 @@
 import { GoogleGenAI } from "@google/genai";
 import "dotenv/config";
-// The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI({});
 
-export default async function Gemini() {
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY
+});
+
+export default async function Gemini(prompt) {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "Fala gemini, quanto é 43 + 43"
+    contents: [
+      { role: "user", parts: [{ text: prompt }] }
+    ]
   });
   console.log(response.text);
   return response.text;
 }
-
-Gemini();
---
+++ JS/testeGemini.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import Gemini from './gemini.js';  
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', async (req, res) => {
+    try {
+        const response = await Gemini("Quanto é 43 + 43?");
+        res.send(`Resposta do Gemini: ${response}`);
+    } catch (error) {
+        res.status(500).send("Erro ao obter resposta do Gemini");
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    
+});
